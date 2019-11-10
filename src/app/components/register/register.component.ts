eb152c716fca7e2ad8aa2ff9c4d50191bf4b8d32
Gal Epshtein <geal2004@gmail.com>
2019-11-10 19:54:28 +0200

    upload profile picture

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,11 @@ import { Component } from "@angular/core";
 import { Location } from "@angular/common";
 const applicationSettings = require('tns-core-modules/application-settings');import {FirebaseService} from '../../services/firebase.service';
 import {User} from '../../models/user.model';
+import * as imagepicker from "nativescript-imagepicker";
+import { Page } from "tns-core-modules/ui/page/page";
+const imageSourceModule = require(`tns-core-modules/image-source`);
+const firebase = require("nativescript-plugin-firebase");
+const fs = require("tns-core-modules/file-system");
 
 @Component({
     moduleId: module.id,
@@ -11,26 +16,71 @@ import {User} from '../../models/user.model';
 export class RegisterComponent {
     isAuthenticating = false;
     public user: User;
+    public list;
+    private userProfilePicURI;
 
-    public constructor(private location: Location, private firebaseService: FirebaseService) {
+    public constructor(private page: Page, private location: Location, private firebaseService: FirebaseService) {
 
         this.user = new User();
+        
     }
 
+    public selectProfilePicture() {
+      const this_ = this;
+      const context = imagepicker.create({ mode: "single"});
+      context.authorize().then(function() {
+      return context.present();
+      }).then(function(selection) {
+      selection.forEach(function(selected) {
+          // process the selected image
+          if (selected.android) {
+            const Pview: any = this_.page.getViewById(`userPic`);
+            Pview.src = selected.android;
+            this_.userProfilePicURI = selected.android;
+          }
+      });
+      this.list = selection;
+      }).catch(function (e) {});
+    }
     public goBack() {
         this.location.back();
     }
     signUp() {
-    this.firebaseService.register(this.user)
-      .then(() => {
-        this.isAuthenticating = false;
-        this.location.back();
-      //  this.toggleDisplay();
-      })
-      .catch((message:any) => {
-        alert(message);
-        this.isAuthenticating = false;
-      });
+      const this_ = this;
+      this.firebaseService.register(this.user)
+        .then((res) => {
+          this.isAuthenticating = false;
+          const userID = JSON.parse(res as string)['uid'];
+          this_.uploadProfilePicToFirebase(userID, this.userProfilePicURI);
+          this.location.back();
+        //  this.toggleDisplay();
+        })
+        .catch((message:any) => {
+          alert(message);
+          this.isAuthenticating = false;
+        });
+}
+
+  uploadProfilePicToFirebase(userID, filePath) {
+    const this_ = this;
+    firebase.storage.uploadFile({
+      // the full path of the file in your Firebase storage (folders will be created)
+      remoteFullPath: 'uploads/usersProfilePics/'+userID+'.jpg',
+      // option 1: a file-system module File object
+      localFile: fs.File.fromPath(filePath),
+      // get notified of file upload progress
+      onProgress: function(status) {
+        console.log("Uploaded fraction: " + status.fractionCompleted);
+        console.log("Percentage complete: " + status.percentageCompleted);
+      }
+    }).then(
+        function (uploadedFile) {
+          console.log("File uploaded: " + JSON.stringify(uploadedFile));
+        },
+        function (error) {
+          console.log("File upload error: " + error);
+        }
+    );
   }
 
 }
